6b872b97f6c08a2ef1ef25abaedaa2f462c5afde
Martin Hare Robertson <mchr3k@googlemail.com>
2010-04-06 20:43:35 +0100

    - Add ignore file - Fix classpath - Add test agent

---
+++ gb.testinstru/src/gb/test/TestApplication.java
@@ -0,0 +1,18 @@
+package gb.test;
+
+public class TestApplication
+{
+
+  /**
+   * @param args
+   */
+  public static void main(String[] args) throws Exception
+  {
+    for (int ii = 0; ii < 10; ii++)
+    {
+      System.out.println(ii);
+      Thread.sleep(1000);
+    }
+  }
+
+}
---
+++ gb.testinstru/src/gb/testinstru/Agent.java
@@ -0,0 +1,21 @@
+package gb.testinstru;
+
+import java.lang.instrument.Instrumentation;
+
+/**
+ * Test Agent
+ */
+public class Agent
+{
+  /**
+   * Agent called on JVM init.
+   * @param agentArgs
+   * @param inst
+   * @throws IOException
+   */
+  public static void premain(String agentArgs, Instrumentation inst)
+  {
+    System.out.println("Loaded Agent.");
+  }
+}
+
