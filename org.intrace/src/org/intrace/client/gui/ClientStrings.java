27fdce28d7d31b34b9de32048e4c89cf44e43d4c
Martin Robertson <mchr3k@googlemail.com>
2010-05-31 09:52:59 +0100

    Update gui strings and update screenshot

--- org.intrace/src/org/intrace/client/gui/ClientStrings.java
+++ org.intrace/src/org/intrace/client/gui/ClientStrings.java
@@ -3,32 +3,22 @@ package org.intrace.client.gui;
 public class ClientStrings
 {
   public static final String ENABLE_INSTR = "Instrument Classes";
-  public static final String DISABLE_INSTR = "Disable Instrumentation";
   public static final String SET_CLASSREGEX = "Set Class Regex...";
   public static final String ENABLE_ALLOWJARS = "Instrument JARs";
-  public static final String DISABLE_ALLOWJARS = "Disable JAR Instrumentation";
   public static final String ENABLE_SAVECLASSES = "Save Instru'd Classes";
-  public static final String DISABLE_SAVECLASSES = "Don't Save Classes";
   public static final String ENABLE_VERBOSEMODE = "Verbose Mode";
-  public static final String DISABLE_VERBOSEMODE = "Disable Verbose Mode";
   public static final String LIST_MODIFIED_CLASSES = "List Instru'd Classes";
 
-  public static final String ENABLE_EE_TRACE = "Enable Entry/Exit Trace";
-  public static final String DISABLE_EE_TRACE = "Disable Entry/Exit Trace";
-  public static final String ENABLE_BRANCH_TRACE = "Enable Branch Trace";
-  public static final String DISABLE_BRANCH_TRACE = "Disable Branch Trace";
-  public static final String ENABLE_ARGS_TRACE = "Enable Args Trace";
-  public static final String DISABLE_ARGS_TRACE = "Disable Args Trace";
+  public static final String ENABLE_EE_TRACE = "Entry/Exit Trace";
+  public static final String ENABLE_BRANCH_TRACE = "Branch Trace";
+  public static final String ENABLE_ARGS_TRACE = "Args Trace";
 
   public static final String BEGIN_CAPTURE_CALLERS = "New Callers Capture...";
   public static final String END_CAPTURE_CALLERS = "End Callers Capture";
 
   public static final String ENABLE_STDOUT_OUTPUT = "StdOut Output";
-  public static final String DISABLE_STDOUT_OUTPUT = "Disable StdOut Output";
   public static final String ENABLE_FILE_OUTPUT = "File Output";
-  public static final String DISABLE_FILE_OUTPUT = "Disable File Output";
   public static final String ENABLE_NETWORK_OUTPUT = "Network Output";
-  public static final String DISABLE_NETWORK_OUTPUT = "Disable Network Output";
 
   public static final String CONNECT = "Connect";
   public static final String CONNECTING = "Connect...";
