beb2534de17c248dd18f9ea8ec856adc246e54ca
Martin Robertson <mchr3k@googlemail.com>
2010-06-01 13:34:56 +0100

    Further PatternInputWindow work - save pattern callback implemented.

--- org.intrace/src/org/intrace/client/gui/PatternInputWindow.java
+++ org.intrace/src/org/intrace/client/gui/PatternInputWindow.java
@@ -3,9 +3,11 @@ package org.intrace.client.gui;
 import net.miginfocom.swt.MigLayout;
 
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.MouseEvent;
 import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.List;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Text;
@@ -17,7 +19,17 @@ public class PatternInputWindow
    */
   public static void main(String[] args)
   {
-    new PatternInputWindow("Test").open();
+    String helpText = "Enter pattern in the form " +
+    		"\"mypack.mysubpack.MyClass\" or using wildcards " +
+    		"\"mypack.*.MyClass\" or \"*MyClass\" etc";
+    new PatternInputWindow("Test",helpText, new PatternInputCallback()
+    {      
+      @Override
+      public void setPattern(String newPattern)
+      {
+        System.out.println(newPattern);
+      }
+    }).open();
   }
   
   public void open()
@@ -35,10 +47,12 @@ public class PatternInputWindow
   private Shell sWindow = null;
   private final Text patternInput; 
   private final List patternSet;
+  private final PatternInputCallback callback;
   
-  public PatternInputWindow(String windowTitle)
+  public PatternInputWindow(String windowTitle, String helpText, PatternInputCallback callback)
   {
-    MigLayout windowLayout = new MigLayout("fill","[grow][60]","[25][grow]");
+    this.callback = callback;
+    MigLayout windowLayout = new MigLayout("fill","[grow][100][100][grow]","[25][grow][25]");
 
     sWindow = new Shell(SWT.APPLICATION_MODAL | SWT.CLOSE | SWT.TITLE | SWT.RESIZE);
     sWindow.setText(windowTitle);
@@ -46,16 +60,39 @@ public class PatternInputWindow
     sWindow.setSize(new Point(400, 300));
     sWindow.setMinimumSize(new Point(400, 300));
     
-    patternInput = new Text(sWindow, SWT.BORDER);
+    Group newPatternGroup = new Group(sWindow, SWT.SHADOW_ETCHED_IN);
+    newPatternGroup.setLayoutData("grow,wrap,spanx");
+    MigLayout newPatternLayout = new MigLayout("fill", "[grow][60]");
+    newPatternGroup.setLayout(newPatternLayout);
+    newPatternGroup.setText("New Pattern");
+    
+    patternInput = new Text(newPatternGroup, SWT.BORDER);
     patternInput.setLayoutData("grow");
     
-    Button addButton = new Button(sWindow, SWT.PUSH);
+    Button addButton = new Button(newPatternGroup, SWT.PUSH);
     addButton.setText("Add");
     addButton.setAlignment(SWT.CENTER);
-    addButton.setLayoutData("grow,wrap");
+    addButton.setLayoutData("grow");
+    addButton.setToolTipText("Help: " + helpText);
     
-    patternSet = new List(sWindow, SWT.BORDER);
-    patternSet.setLayoutData("spanx,grow");
+    Group patternGroup = new Group(sWindow, SWT.SHADOW_ETCHED_IN);
+    patternGroup.setLayoutData("grow,spanx,wrap");
+    MigLayout patternLayout = new MigLayout("fill");
+    patternGroup.setLayout(patternLayout);
+    patternGroup.setText("Patterns (Double click to Edit)");
+    
+    patternSet = new List(patternGroup, SWT.BORDER);
+    patternSet.setLayoutData("grow");
+    
+    Button saveButton = new Button(sWindow, SWT.PUSH);
+    saveButton.setText("Save Changes");
+    saveButton.setAlignment(SWT.CENTER);
+    saveButton.setLayoutData("skip,grow");
+    
+    Button discardButton = new Button(sWindow, SWT.PUSH);
+    discardButton.setText("Discard Changes");
+    discardButton.setAlignment(SWT.CENTER);
+    discardButton.setLayoutData("grow");
     
     addButton.addMouseListener(new org.eclipse.swt.events.MouseAdapter()
     {
@@ -65,6 +102,31 @@ public class PatternInputWindow
         addItem();
       }
     });
+    patternSet.addMouseListener(new org.eclipse.swt.events.MouseAdapter() 
+    {
+      @Override
+      public void mouseDoubleClick(MouseEvent mouseevent)
+      {
+        editItem();
+      }}
+    );
+    saveButton.addMouseListener(new org.eclipse.swt.events.MouseAdapter() 
+    {
+      @Override
+      public void mouseUp(MouseEvent mouseevent)
+      {
+        savePattern();
+        sWindow.close();
+      }}
+    );
+    discardButton.addMouseListener(new org.eclipse.swt.events.MouseAdapter() 
+    {
+      @Override
+      public void mouseUp(MouseEvent mouseevent)
+      {
+        sWindow.close();
+      }}
+    );
   }
   
   private void addItem()
@@ -88,5 +150,39 @@ public class PatternInputWindow
       }
       patternInput.setText("");
     }
+  }
+  
+  private void editItem()
+  {
+    int[] selectedItems = patternSet.getSelectionIndices();
+    
+    if (selectedItems.length == 1)
+    {
+      patternInput.setText(patternSet.getItem(selectedItems[0]));
+      patternSet.remove(selectedItems[0]);
+    }
+  }
+  
+  private void savePattern()
+  {    
+    StringBuffer pattern = new StringBuffer("");
+    String patternStr = pattern.toString();
+    String[] items = patternSet.getItems();
+    if (items.length > 0)
+    {
+      for (String item : items)
+      {
+        pattern.append(item.replace(".", "\\.").replace("*", ".*"));
+        pattern.append("|");
+      }
+      patternStr = pattern.toString();
+      patternStr = patternStr.substring(0, patternStr.length() - 1);
+    }
+    callback.setPattern(patternStr);
+  }
+  
+  public static interface PatternInputCallback
+  {
+    public void setPattern(String newPattern);
   }
 }
