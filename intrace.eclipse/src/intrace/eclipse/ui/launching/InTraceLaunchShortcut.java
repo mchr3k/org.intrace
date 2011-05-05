9385ec9b6c906638ad860b788f353048d488b033
mchr3k <mchr3k@googlemail.com>
2011-05-05 13:32:22 +0100

    Fix launching Java Application

--- intrace.eclipse/src/intrace/eclipse/ui/launching/InTraceLaunchShortcut.java
+++ intrace.eclipse/src/intrace/eclipse/ui/launching/InTraceLaunchShortcut.java
@@ -7,18 +7,19 @@ import org.eclipse.ui.IEditorPart;
 public class InTraceLaunchShortcut implements ILaunchShortcut
 {
 
+  // Need to do most of the same as 
+  // org.eclipse.jdt.internal.debug.ui.launcher.JavaApplicationLaunchShortcut
+  
   @Override
   public void launch(ISelection selection, String mode)
   {
     // TODO Auto-generated method stub
-
   }
 
   @Override
   public void launch(IEditorPart editor, String mode)
   {
     // TODO Auto-generated method stub
-
   }
 
 }
