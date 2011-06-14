cc1e3cfdc7e4090c6365c05deea8dff5f940cc2e
mchr3k <mchr3k@gmail.com>
2011-06-14 09:51:35 +0100

    Remove debug layout

--- org.intrace/src/org/intrace/client/gui/helper/InTraceUI.java
+++ org.intrace/src/org/intrace/client/gui/helper/InTraceUI.java
@@ -860,7 +860,7 @@ public class InTraceUI implements ISocketCallback, IControlConnectionListener
 
     private TextOutputTab(Composite parent)
     {
-      MigLayout windowLayout = new MigLayout("debug,fill,wmin 0,hmin 0",
+      MigLayout windowLayout = new MigLayout("fill,wmin 0,hmin 0",
           "[100][100][100][150][100][grow]", "[][][grow][]");
 
       composite = new Composite(parent, SWT.NONE);
